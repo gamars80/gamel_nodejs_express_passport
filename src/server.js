d1b3e29a93064a8de54db43d5e8fcecfd30c19fe
gamars <gamars80@gmail.com>
2023-11-14 15:33:30 +0900

    fix: 몽고디비 주소 감춤

--- src/server.js
+++ src/server.js
@@ -3,6 +3,15 @@ const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
 const User = require('./models/users.model');
+const passport = require('passport');
+
+
+//패스포트를 이용하기위한 미들웨어 등록
+app.use(passport.initialize());
+app.use(passport.session());
+
+//분리한 passort.js 를 require한다
+require('./config/passport');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -16,7 +25,7 @@ app.set('view engine', 'ejs');
 
 //몽고 커넥션
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://gamars80:qwer@cluster0.aaa1s7u.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`dddd`)
 .then(() => {
     console.log('mongodb connected');
     
@@ -48,6 +57,25 @@ app.post('/signup', async (req, res) => {
     }
 })
 
+app.post('/login', (req, res, next) => {
+    passport.authenticate("local", (err, user , info) => {
+        //에러가 있으면 express 에러처리기로 next
+        if(err) next(err); 
+
+        //유저가 없으면 info로 넘어온 메세지를 전달
+        if(!user) {
+            return res.json({msg:info});
+        }
+        
+        //passport에서 제공하는 login
+        req.logIn(user , function(err){
+            if(err) return next(err);
+
+            res.redirect('/')
+        })
+
+    })
+})
 const port = 8080;
 app.listen(port, (req, res) => {
     console.log(`Listening On ${port}`);
